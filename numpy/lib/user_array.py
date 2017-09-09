40a077a42939ab7c49a901ab16f70961779bd9f7
U-TRINGUYEN\51204 <minhtribk12@gmail.com>
2017-09-09 16:57:06 +0700

    log add function

--- numpy/lib/user_array.py
+++ numpy/lib/user_array.py
@@ -67,7 +67,7 @@ class container(object):
 
     def __iadd__(self, other):
         with open("/home/minhtri/workspace/numpy_test/workspace/log/log7.txt","a") as file_log: 
-            file_log.write("run time: {}\n".format(end)) 
+            file_log.write("iadd is used \n") 
         add(self.array, other, self.array)
         return self
 
